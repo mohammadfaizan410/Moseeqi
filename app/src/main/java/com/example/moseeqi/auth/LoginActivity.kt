72cb2cd6af230b7a3f93c31d26ae3a998343db13
Muhammad Faizan <40329896+mohammadfaizan410@users.noreply.github.com>
2024-05-06 15:30:10 +0300

    first

---
+++ app/src/main/java/com/example/moseeqi/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moseeqi.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.moseeqi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login) // Assuming your layout file is named "login.xml"
+    }
+}
---
+++ app/src/main/java/com/example/moseeqi/auth/RegisterActivity.kt
@@ -0,0 +1,4 @@
+package com.example.moseeqi.auth
+
+class RegisterActivity {
+}
