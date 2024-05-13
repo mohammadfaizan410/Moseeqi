1d8f9a90d64c18532d888e044ad4793422fd7b31
Ayesha <ayeshanizam510@gmail.com>
2024-05-13 19:12:19 +0300

    halo people. settings page added

--- app/src/main/java/com/example/moseeqi/MainActivity.kt
+++ app/src/main/java/com/example/moseeqi/MainActivity.kt
@@ -5,12 +5,15 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.room.Room
 import com.ctis487.roomdatabasewithonetomanyrelation.db.UserRoomDatabase
 import com.ctis487.roomdatabasewithonetomanyrelation.db.UserWithPlaylists
 import com.example.moseeqi.auth.LoginActivity
+import com.example.moseeqi.ayesha.AppSettingsActivity
 import com.example.moseeqi.constants.Constants
 import com.example.moseeqi.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,25 +31,49 @@ class MainActivity : AppCompatActivity() {
                 .build()
         }
         setContentView(binding.root)
-        sharedPreferences = getSharedPreferences(Constants.SHARED_PREFS, Context.MODE_PRIVATE)
+//        sharedPreferences = getSharedPreferences(Constants.SHARED_PREFS, Context.MODE_PRIVATE)
+//
+//        if(sharedPreferences.getString(Constants.USERNAME_KEY,"none") == "none"){
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//        }else {
+//            var allUsers = userDB.playListDao().getUsersWithPlaylists();
+//            allUsers.forEach {
+//                if(it.user.username ==  sharedPreferences.getString(Constants.USERNAME_KEY,"none")){
+//                    println("user is already logged in");
+//                }
+//            }
+//        }
+//
+//        binding.login.setOnClickListener {
+//            binding.login.setOnClickListener {
+//                val intent = Intent(this, LoginActivity::class.java)
+//                startActivity(intent)
+//            }
+//        }
+        var bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener {
 
-        if(sharedPreferences.getString(Constants.USERNAME_KEY,"none") == "none"){
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }else {
-            var allUsers = userDB.playListDao().getUsersWithPlaylists();
-            allUsers.forEach {
-                if(it.user.username ==  sharedPreferences.getString(Constants.USERNAME_KEY,"none")){
-                    println("user is already logged in");
+            try {
+                when (it.itemId) {
+                    R.id.homeNavbtn -> {
+
+                        true
+                    }
+                    R.id.settingsNavbtn -> {
+                        val intent = Intent(this, AppSettingsActivity::class.java)
+                        startActivity(intent)
+                        true
+                    } else -> {
+                    true
                 }
-            }
-        }
-
-        binding.login.setOnClickListener {
-            binding.login.setOnClickListener {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
+                }
+            } catch (e : Exception) {
+                throw e
             }
         }
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/moseeqi/ayesha/AppSettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moseeqi.ayesha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moseeqi.R
+
+
+class AppSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_settings)
+    }
+}
